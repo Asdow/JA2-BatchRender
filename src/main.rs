af7bd7707bb094a0d5ed0b559af0038f9190b41d
Asdow <20314541+Asdow@users.noreply.github.com>
2021-12-16 21:37:07 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+#![allow(non_snake_case)]
+use std::env;
+use std::process;
+use std::path::Path;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+
+    
+    println!("Rendering Jagged Alliance 2 animations\n");
+    
+    //  Count amount of generated script files
+    let currentDir = env::current_dir().unwrap();
+
+    let mut extractPath = currentDir.clone();
+    extractPath.push("renderGeneratedScripts");
+    let dirExists: bool = extractPath.is_dir();
+    if !dirExists
+    {
+        println!("Directory for generated scripts not found at: {}", extractPath.to_string_lossy());
+        process::exit(2)
+    }
+
+    let mut count = 0;
+    loop{
+        let scriptFilename = config.animationFilename.replace(".py", &format!("{}.py", count));
+        let mut pythonFile = currentDir.clone();
+        pythonFile.push("renderGeneratedScripts");
+        pythonFile.push(format!("{}", scriptFilename));
+
+        let result = Path::new(&pythonFile).exists();
+        match result {
+            true => {count += 1; continue},
+            false => break
+        }
+    }
+    if count == 0
+    {
+        println!("No generated render scripts {} found at: {}", config.animationFilename, extractPath.to_string_lossy());
+        process::exit(3);
+    }
+
+
+    // Call blender
+    let mut processes: Vec<process::Child> = Vec::new();
+    let mut blendfileDir = currentDir.clone();
+    blendfileDir.push(config.blendFilename);
+
+    for i in 0..count
+    {
+        let scriptFilename = config.animationFilename.replace(".py", &format!("{}.py", i));
+        let mut pythonFile = currentDir.clone();
+        pythonFile.push("renderGeneratedScripts");
+        pythonFile.push(format!("{}", scriptFilename));
+
+        let blenderPath = format!("{}\\blender.exe", config.blenderDir);
+        println!("{}", blenderPath);
+
+        let com = process::Command::new(blenderPath)
+        .args(&[
+            "-b", blendfileDir.to_str().unwrap(),
+            "-P", pythonFile.to_str().unwrap()
+            ])
+            .stdout(process::Stdio::null())
+            .spawn().unwrap();
+        processes.push(com);
+    }
+
+
+    // Wait for the renders to finish
+    for mut child in processes
+    {
+        child.wait().expect("blender.exe wasn't running");
+    }
+
+    println!("Rendering complete!");
+    process::exit(0)
+}
+
+
+struct Config {
+    blenderDir: String,
+    blendFilename: String,
+    animationFilename: String
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 4 {
+            let errString = String::from("Not enough arguments!\nArguments must be path to Blender, blender filename and filename for python script\nEg. ") + &args[0] + ".exe \"C:\\Blender\\Blender 3.0\" \"JA2 2.9_033.blend\" \"batchrender-rifleAnims.py\"";
+            return Err(errString);
+        }
+
+        let blenderDir = args[1].clone();
+        let blendFilename = args[2].clone();
+        let animationFilename = args[3].clone();
+
+        Ok(Config {blenderDir, blendFilename, animationFilename})
+    }
+}
